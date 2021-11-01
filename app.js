d5c8530a9afcefc0f1b1c981038e65fd04875287
jnhck <s2jaheuc@uni-trier.de>
2021-11-01 20:48:27 +0100

    already works with the timeservice atm

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require("express"); // express for building the web app
+const express = require("express");
+const axios = require("axios");
 const app = express();
-const path = require('path');
 
 app.use(express.static("public"));
 
@@ -10,7 +10,31 @@ app.listen(port, () => {
   console.log("Listening at port:" + port);
 });
 
+app.get("/api/timezone/", function(req, res) { 
+  const timezone = req.query.tz;
+  const requestURL = "http://time-microservice:4000/api/timezone/?tz=" + timezone;
+
+  var config = {
+    method: 'get',
+    url: requestURL,
+    headers: {}
+  };
+  
+  axios(config)
+  .then(function (response) {
+    res.send(response.data); // we just forward the entire .json
+  })
+  .catch(function (error) {
+    if (error.response) {
+      res.status(404).send("Error occurred! Reason: " + error.response.data)
+    }
+    else {
+      res.status(404).send("Seems like the timezone container is offline...")
+    }
+  });
+});
+
 app.get("*", function(req, res) {
-  res.sendFile("index.html");
+  res.sendFile("public/index.html", { root: __dirname });
 });
 
