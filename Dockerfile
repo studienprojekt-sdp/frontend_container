fcdac6a0b09d7af8a32361be126c6f8c2f2d03d5
jnhck <s2jaheuc@uni-trier.de>
2021-11-02 15:20:12 +0100

    ensuring compatibility with other services

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,3 @@ RUN npm install
 COPY . .
 
 ENTRYPOINT [ "node", "app" ]
-
-
-
--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static("public"));
 const port = 3000;
 
 app.listen(port, () => {
-  console.log("Listening at port:" + port);
+  console.log("Frontend container is running and listening at port: " + port);
 });
 
 app.get("/api/timezone/", function(req, res) { 
@@ -22,7 +22,7 @@ app.get("/api/timezone/", function(req, res) {
   
   axios(config)
   .then(function (response) {
-    res.send(response.data); // we just forward the entire .json
+    res.send(response.data);
   })
   .catch(function (error) {
     if (error.response) {
@@ -30,6 +30,30 @@ app.get("/api/timezone/", function(req, res) {
     }
     else {
       res.status(404).send("Seems like the timezone container is offline...")
+    }
+  });
+});
+
+app.get("/api/weather/", function(req, res) { 
+  const city = req.query.c;
+  const requestURL = "http://weather-microservice:4001/api/weather/?c=" + city;
+
+  var config = {
+    method: 'get',
+    url: requestURL,
+    headers: {}
+  };
+  
+  axios(config)
+  .then(function (response) {
+    res.send(response.data);
+  })
+  .catch(function (error) {
+    if (error.response) {
+      res.status(404).send("Error occurred! Reason: " + error.response.data)
+    }
+    else {
+      res.status(404).send("Seems like the weather container is offline...")
     }
   });
 });
