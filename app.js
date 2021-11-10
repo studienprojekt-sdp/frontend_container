0b74d4511d23720adbd65f30730ab4bda5f9821a
jnhck <s2jaheuc@uni-trier.de>
2021-11-10 10:20:34 +0100

    better http status responses + other minor tweaks

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const axios = require("axios");
+import express from "express";
+import axios from "axios";
+import path from "path";
+
 const app = express();
+const port = 3000;
 
 app.use(express.static("public"));
-
-const port = 3000;
 
 app.listen(port, () => {
   console.log("Frontend container is running and listening at port: " + port);
@@ -22,14 +23,14 @@ app.get("/api/timezone/", function(req, res) {
   
   axios(config)
   .then(function (response) {
-    res.send(response.data);
+    res.status(200).send(response.data);
   })
   .catch(function (error) {
     if (error.response) {
-      res.status(404).send("Error occurred! Reason: " + error.response.data)
+      res.status(504).send("Error occurred! Reason: " + error.response.data)
     }
     else {
-      res.status(404).send("Seems like the timezone container is offline...")
+      res.status(504).send("The time container cannot be reached. Check if it is online.")
     }
   });
 });
@@ -46,19 +47,19 @@ app.get("/api/weather/", function(req, res) {
   
   axios(config)
   .then(function (response) {
-    res.send(response.data);
+    res.status(200).send(response.data);
   })
   .catch(function (error) {
     if (error.response) {
-      res.status(404).send("Error occurred! Reason: " + error.response.data)
+      res.status(504).send("Error occurred! Reason: " + error.response.data)
     }
     else {
-      res.status(404).send("Seems like the weather container is offline...")
+      res.status(504).send("The weather container cannot be reached. Check if it is online.")
     }
   });
 });
 
 app.get("*", function(req, res) {
-  res.sendFile("public/index.html", { root: __dirname });
+  res.status(200).sendFile("public/index.html", { root: path.resolve(path.dirname('')) });
 });
 
